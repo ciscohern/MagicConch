15e0b94bec4c4f104086f2e3754258fe806c2a06
Francisco Hernandez <cisco52096@gmail.com>
2019-11-21 00:03:58 -0800

    Twitter Bot

---
+++ test.py
@@ -0,0 +1,12 @@
+import random
+
+conch = [' Maybe someday.', ' I dont think so.', ' Yes.', ' Try asking again.', ' No.', ' Maybe someday.', 
+		' It is certain.', ' It is decidedly so.',' Without a doubt.',' Yes - definitely.',' You may rely on it.',
+		' As I see it, yes.',' Most likely.',' Outlook good.',' Signs point to yes.',' Reply hazy, try again.',
+		' Ask again later.', ' Better not tell you now.',' Cannot predict now.', ' Concentrate and ask again.',
+		' Dont count on it.',' My reply is no.', ' My sources say no.',' Outlook not so good.', ' Very doubtful.']
+
+
+
+random = random.choice(conch)
+print(random)
